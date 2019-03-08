8b4c35c7e93a56b617916ad39306cae261f6eb42
jacky778199 <ting825030@gmail.com>
2019-03-08 12:29:18 +0800

    完成基本網頁架構 / 設定模式 /status 目前設定情況 /Node-status 因為Node要設定資料長度再加一個頁面

--- ESP8266_Field_Test_Project.ino
+++ ESP8266_Field_Test_Project.ino
@@ -7,6 +7,7 @@ const char* ssid = "esp8266_OLED";
 const char* passphrase = "12345678";
 String st;
 String content;
+String data_length, set_freq, set_sf , set_mode;
 int statusCode;
 
 void setup() {
@@ -49,28 +50,45 @@ void createWebServer(int webtype)
 			String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
 			content = "<!DOCTYPE HTML>\r\n<html>Hello from Field test tool at ";
 			content += ipStr;
-		
-			
-			content += "<fieldset>\r\n <legend> Set RF Parameter </legend>";
-			content += "<form method='get' action='status'> ";
-			content += "<label>Freq: </label><input name='freq' length=32>" ;
-			content += "<label>SF = < / label><select name = 'sf' length = 32>" ;
-			content += "<label>Mode= </label><input name ='mode' length = 32>" ;
-			content += "<input type = 'submit'>< / form>";
-			content += "</fieldset>";
-			content += "</html>";
+			content += "<hr>";
+			content += "<fieldset> <legend> Set RF Parameter </legend> ";
+			content += "<form method='get' action='status'>   ";
+			content += "<label>Freq: </label> <input type=''text' name='freq' size='5'> Mhz<br/ > ";
+			content += "<label>SF= </label> <select name='sf'> <option value='12' selected='selected'>SF12  <option value='11'>SF11 <option value='10'>SF10 <option value='9'>SF9 <option value='8'>SF8 <option value='7'>SF7 <option value='6'>SF6 </option> </select> <br/ > ";
+			content += "<label>Mode= </label> <input type='radio' name='mode' value='gw' checked='checked' /> Gateway <input type='radio' name='mode' value='node' /> Node <br/ >  ";
+			content += "<input type = 'submit'></form>  ";
+			content += "</fieldset> </html>  ";
 
 			server.send(200, "text/html", content);
 		});
 		server.on("/status", []() {
-			String set_freq = server.arg("freq");
-			String set_bw = server.arg("bw");
-			String set_mode = server.arg("mode");
-			if (set_freq.length() > 0 )
+			set_freq = server.arg("freq");
+			set_sf = server.arg("sf");
+			set_mode = server.arg("mode");
+			if (set_mode=="gw" )
 			{
-				content = "{\"Success\":\"saved setting parameter...set_freq= ";
+				content = "<< Gateway Mode >> Parameter <hr> <br/> Freq= ";
+				content += set_freq ;
+				content += "Mhz <br/ > SF= ";
+				content += set_sf ;
+				content += "  Mode= ";
+				content += set_mode ;
+				content += "<br/ > <input type='button' onclick ='history.back()' value ='back'></input> ";
+				statusCode = 200;
+			}
+			else if (set_mode == "node")
+			{
+				content = "<< Node Mode >> Parameter <hr> <br/> Freq= ";
 				content += set_freq;
-				content += "<input type='button' onclick ='history.back()' value ='back'></input> ";
+				content += "Mhz <br/ > SF= ";
+				content += set_sf;
+				content += "  Mode= ";
+				content += set_mode;
+				content += "<form method='get' action='Node-status'>   ";
+				content += "<br/> <label>Data Length = </label> <select name = 'data_length'> <option value = '100' selected = 'selected'>100 Bytes  <option value = '1'>1 Bytes <option value = '5'>5 Bytes <option value = '10'>10 Bytes <option value = '50'>50 Bytes <option value = '150'>150 Bytes <option value = '200'>200 Bytes <option value = '241'>241 Bytes </option> </select> <br/> ";
+				content += "<input type = 'submit'></form>  ";
+				content += "<br/ > <a href='/'> Setup page </a> ";
+				
 				statusCode = 200;
 			}
 			else {
@@ -80,6 +98,26 @@ void createWebServer(int webtype)
 			}
 			server.send(statusCode, "text/html", content);
 		});
+
+		server.on("/Node-status", []() {
+			data_length = server.arg("data_length");
+			content = "<< Node Mode >> Parameter <hr> <br/> Freq= ";
+			content += set_freq;
+			content += "Mhz <br/ > SF= ";
+			content += set_sf;
+			content += "  Mode= ";
+			content += set_mode;
+			content += "<b>  Data length= ";
+			content += data_length;
+			
+		//	content += "<br/ > <a href='/status'> Status page </a> ";
+			content += "</b> <br/ > <input type='button' onclick ='history.back()' value ='back'></input> ";
+			content += " <br/ > <a href='/'> Start page </a> ";
+			server.send(200, "text/html", content);
+		});
+
+
+
 	}
 
 }
